ea2319b308673d9cfef10dc671662b8feb239425
Stratos Paraskevaidis <stratosiii@gmail.com>
2015-10-22 20:33:24 +0300

    Express Client#{Job,File}Retention to days instead of seconds

--- app/models/client.rb
+++ app/models/client.rb
@@ -9,4 +9,23 @@ class Client < ActiveRecord::Base
   alias_attribute :job_retention, :JobRetention
 
   has_many :jobs, foreign_key: :ClientId
+
+  DAY_SECS = 60 * 60 * 24
+
+  # Helper method. It shows the client's  job retention,
+  # (which is expressed in seconds) in days.
+  #
+  # @return [Integer]
+  def job_retention_days
+    job_retention / DAY_SECS
+  end
+
+
+  # Helper method. It shows the client's  file retention,
+  # (which is expressed in seconds) in days.
+  #
+  # @return [Integer]
+  def file_retention_days
+    file_retention / DAY_SECS
+  end
 end
