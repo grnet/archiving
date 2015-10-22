90d8e0065b990dc2096f14735cac0d22ee7ea1b3
Stratos Paraskevaidis <stratosiii@gmail.com>
2015-10-22 23:04:51 +0300

    Add helper method for better big number representation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Custom helper for better display of big numbers
+  # @example number_by_magnitude(4242)
+  #  "4.2K"
+  #
+  # @param number[Numeric]
+  # @return [String] human friendly respresentation
+  def number_by_magnitude(number)
+    number_to_human(number, units: { thousand: :K, million: :M, billion: :G })
+  end
 end
