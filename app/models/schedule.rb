7e861666551f3045b89de95b1c41a6bf463ea5d4
Stratos Paraskevaidis <stratosiii@gmail.com>
2015-10-31 12:28:30 +0200

    Schedule is initialized by specifying only the runtime

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,12 +1,37 @@
 class Schedule < ActiveRecord::Base
+  DEFAULT_RUNS = [
+    'Level=Full 1st sun at ',
+    'Level=Differential 2nd-5th sun at ',
+    'Level=Incremental mon-sat at '
+  ]
+
+  attr_accessor :runtime
+
   serialize :runs, JSON
 
   validates :name, :runs, presence: true
+
+  before_validation :set_runs, if: Proc.new { |s| s.runtime.present? }
 
   def to_bacula_config_array
     ['Schedule {'] +
       ["  Name = \"#{name}\""] +
       runs.map {|r| "  Run = #{r}" } +
       ['}']
+  end
+
+  private
+
+  def set_runs
+    if valid_runtime?
+      self.runs = DEFAULT_RUNS.map { |r| r + runtime }
+    else
+      self.errors.add(:runtime, :not_valid_24h_time)
+      false
+    end
+  end
+
+  def valid_runtime?
+    runtime && runtime[/^([01]?[0-9]|2[0-3]):[0-5][0-9]$/]
   end
 end
