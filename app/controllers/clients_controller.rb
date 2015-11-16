917b33b942bbe8d2df384bbec631dd8f70209d54
Stratos Paraskevaidis <stratosiii@gmail.com>
2015-11-16 21:24:10 +0200

    Add charts in clients index and show page
    * jobs' finish status for the user's clients.
    * jobs bytes size and files count

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -3,18 +3,28 @@ class ClientsController < ApplicationController
 
   # GET /clients
   def index
-    client_ids = Client.for_user(current_user.id).pluck(:ClientId)
-    @clients = Client.where(ClientId: client_ids).includes(:jobs)
-    @active_jobs = Job.running.where(ClientId: client_ids).group(:ClientId).count
+    @client_ids = Client.for_user(current_user.id).pluck(:ClientId)
+    @clients = Client.where(ClientId: @client_ids).includes(:jobs)
+    @active_jobs = Job.running.where(ClientId: @client_ids).group(:ClientId).count
     @hosts = current_user.hosts.not_baculized
+    get_charts
   end
 
   # GET /clients/1
-  def show; end
+  def show
+    @client_ids = [@client.id]
+    get_charts
+  end
 
   private
 
   def set_client
     @client = Client.for_user(current_user.id).find(params[:id])
+  end
+
+  def get_charts
+    days_ago = params.fetch(:days_back, 7).to_i rescue 7
+    @job_status = ChartGenerator.job_statuses(@client_ids, days_ago)
+    @job_stats = ChartGenerator.job_stats(@client_ids, days_ago - 1)
   end
 end
