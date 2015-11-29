7d2d63f1558122f1cbc89f57ab158ec09f624e90
Stratos Paraskevaidis <stratosiii@gmail.com>
2015-11-29 11:02:07 +0200

    Add documentation for Schedule class

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,6 @@
+# Schedule model is the application representation of Bacula's Schedule.
+# It has references to a host and multiple schedule run in order to provide
+# the desired Bacula configuration
 class Schedule < ActiveRecord::Base
   has_many :schedule_runs
 
@@ -9,6 +12,9 @@ class Schedule < ActiveRecord::Base
 
   accepts_nested_attributes_for :schedule_runs
 
+  # Constructs an array where each element is a line for the Schedule's bacula config
+  #
+  # @return [Array]
   def to_bacula_config_array
     ['Schedule {'] +
       ["  Name = \"#{name_for_config}\""] +
@@ -16,6 +22,11 @@ class Schedule < ActiveRecord::Base
       ['}']
   end
 
+  # Generates a name that will be used for the configuration file.
+  # It is the name that will be sent to Bacula through the configuration
+  # files.
+  #
+  # @return [String]
   def name_for_config
     [host.name, name].join(' ')
   end
