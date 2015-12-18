a0ca720e57a5f43ceabb7aa0ef748edd2ef90208
Stratos Paraskevaidis <stratosiii@gmail.com>
2015-12-18 19:36:01 +0200

    Introduce mailer
    Mails are not yet dispatched from production.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings =
+    YAML::load(File.open("#{Rails.root}/config/mailer.yml"))[Rails.env].symbolize_keys
 end
