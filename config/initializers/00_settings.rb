d2d38a099aada30fb31e7ea5504146ad20bcfb30
Stratos Paraskevaidis <stratosiii@gmail.com>
2016-04-01 11:56:47 +0300

    Default pool is now a configuration setting

--- config/initializers/00_settings.rb
+++ config/initializers/00_settings.rb
@@ -1,5 +1,7 @@
 Archiving.settings director_name: YAML.load_file(Rails.root.join('config', 'bacula.yml'))[Rails.env].
   symbolize_keys[:director]
+Archiving.settings default_pool: YAML.load_file(Rails.root.join('config', 'bacula.yml'))[Rails.env].
+  symbolize_keys[:pool]
 
 Archiving.settings vima_oauth_enabled: true
 Archiving.settings institutional_authentication_enabled: true
