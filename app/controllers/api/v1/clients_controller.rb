4a395c10ebb0a66438f6ebad36cddef34aa17d29
Stratos Paraskevaidis <stratosiii@gmail.com>
2016-04-24 13:45:39 +0300

    Implement api/clients/:id/backup endpoint

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -14,4 +14,18 @@ class Api::V1::ClientsController < Api::BaseController
 
     api_render(host)
   end
+
+  # POST /api/clients/1/backup
+  def backup
+    host = current_api_user.hosts.in_bacula.find(params[:id])
+    job = host.job_templates.enabled.backup.find(params[:job_id])
+
+    if job.backup_now
+      message = 'Job is scheduled for backup'
+    else
+      message = 'Job can not be scheduled'
+    end
+
+    render json: { message: message }, status: :ok
+  end
 end
