6e0bd01a3af8985156775dbc672298d253448df9
Stratos Paraskevaidis <stratosiii@gmail.com>
2016-09-11 00:15:09 +0300

    Blocked clients can not perform API actions.
    Disabled actions are:
    
    * Backup
    * Restore

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::ClientsController < Api::BaseController
   before_action :require_api_login
+  before_action :fetch_host, only: [:show, :backup, :restore]
+  before_action :require_non_blocked_client, only: [:backup, :restore]
 
   # GET /api/clients
   def index
@@ -10,15 +12,12 @@ class Api::V1::ClientsController < Api::BaseController
 
   # GET /api/clients/1
   def show
-    host = current_api_user.hosts.in_bacula.find(params[:id])
-
-    api_render(host)
+    api_render(@host)
   end
 
   # POST /api/clients/1/backup
   def backup
-    host = current_api_user.hosts.in_bacula.find(params[:id])
-    job = host.job_templates.enabled.backup.find(params[:job_id])
+    job = @host.job_templates.enabled.backup.find(params[:job_id])
 
     if job.backup_now
       message = 'Job is scheduled for backup'
@@ -31,16 +30,28 @@ class Api::V1::ClientsController < Api::BaseController
 
   # POST /api/clients/1/restore
   def restore
-    host = current_api_user.hosts.in_bacula.find(params[:id])
-    fileset = host.client.file_sets.find(params[:fileset_id])
+    fileset = @host.client.file_sets.find(params[:fileset_id])
     location = params[:location].blank? ? '/tmp/bacula_restore' : params[:location]
 
-    if host.restore(fileset.id, location)
+    if @host.restore(fileset.id, location)
       message = 'Restore is scheduled'
     else
       message = 'Restore not scheduled'
     end
 
     render json: { message: message }, status: :ok
+  end
+
+  private
+
+  def fetch_host
+    @host = current_api_user.hosts.in_bacula.find(params[:id])
+  end
+
+  def require_non_blocked_client
+    if @host.blocked?
+      message = 'Client is disabled by admins'
+      render json: { message: message }, status: :ok
+    end
   end
 end
