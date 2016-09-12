d6f32ef1b5661a204dabd5e02e3ecd64e7117519
Stratos Paraskevaidis <stratosiii@gmail.com>
2016-09-12 21:31:00 +0300

    Enable emails on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings =
     YAML::load(File.open("#{Rails.root}/config/mailer.yml"))[Rails.env].symbolize_keys
 
