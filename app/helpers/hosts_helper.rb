46817e56aa28fb5c4daa71368b3cedb6691f7452
Stratos Paraskevaidis <stratosiii@gmail.com>
2016-11-19 21:49:15 +0200

    Provide helpful messages on client state label

--- app/helpers/hosts_helper.rb
+++ app/helpers/hosts_helper.rb
@@ -6,24 +6,35 @@ module HostsHelper
     case
     when host.pending?
       klass = "default"
+      msg = 'Client is not configured yet. No backups available'
     when host.configured?
       klass = "warning"
+      msg = 'Client is configured. Configuration not yet sent to the backup server. No backups available'
     when host.dispatched?
       klass = "info"
+      msg = 'Client is configured. Configuration not yet sent to the backup server. No backups available'
     when host.deployed?
       klass = "success"
+      msg = 'Client is OK'
     when host.updated?
       klass = "info"
+      msg = 'Client has new configuration, not yet transmitted to the server. Backups are available for the previous config'
     when host.redispatched?
       klass = "primary"
+      msg = 'Client has new configuration, not yet transmitted to the server. Backups are available for the previous config'
     when host.for_removal?
       klass = "danger"
+      msg = 'Client will be removed'
     when host.inactive?
       klass = "warning"
+      msg = 'Client is not yet verified'
     when host.blocked?
       klass = "warning"
+      msg = 'Client is blocked by an administrator. No backups available'
     end
 
-    content_tag(:span, class: "label label-#{klass}") { host.human_status_name.upcase }
+    content_tag(:small, 'data-toggle' => 'tooltip', title: msg) do
+      content_tag(:span, class: "label label-#{klass}") { host.human_status_name.upcase }
+    end
   end
 end
