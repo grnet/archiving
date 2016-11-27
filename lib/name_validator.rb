f67e7e74a3821ea76ed06907ff11d6719277c712
Stratos Paraskevaidis <stratosiii@gmail.com>
2016-11-27 12:22:49 +0200

    Name validator can accept '-'

--- lib/name_validator.rb
+++ lib/name_validator.rb
@@ -1,6 +1,6 @@
 class NameValidator < ActiveModel::Validator
   def validate(record)
-    unless record.name =~ /^[a-zA-Z0-1\.-_ ]+$/
+    unless record.name =~ /^[a-zA-Z0-9\.\-_ ]+$/
       record.errors[:name] << "Can only contain numbers, letters, space, '.', '-' and '_'"
     end
   end
