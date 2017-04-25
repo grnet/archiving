84aa1987105d2efec014da6354e554b8269f1d68
Kostis Fardelas <kosfar@noc.grnet.gr>
2017-04-25 13:19:30 +0300

    Custom ssh port for cap production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,4 +4,7 @@ set :repo_url, 'https://repo.grnet.gr/diffusion/ARCHIVING/archiving.git'
 
 server '83.212.1.103', user: 'archiving', roles: %w(web app db)
 
-set :ssh_options, forward_agent: false
+set :ssh_options, {
+    forward_agent: false,
+    port: 29
+}
